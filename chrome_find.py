84b0c04d67c8d4b0d6ebe16cdf2c4428e8cb3651
Gabriel Staples <ercaguy@gmail.com>
2020-07-03 18:09:57 -0700

    chrome_find.py: update some comments, add hashbang

---
+++ chrome_find.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" 
+
+Associate this script with an **operating system shortcut** (whether on Windows, Mac, or Linux),
+such as Ctrl + Shift + F. Press that shortcut while in Chrome and it will run this script, which 
+will run the case-insensitive-search Javascript program in Chrome by pressing Ctrl + L to go to 
+the address bar and then typing `657search198` to activate your custom search you should have
+already attached to this Javascript script.
+
+"""
